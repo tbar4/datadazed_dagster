b8c3bc165f1714db3e3d32dbbb834acd30f979cf
tbarnes <trevor.barnes91@gmail.com>
2024-03-05 16:22:07 -0800

    add ghost capability

--- spacedevs/assets/prod_news/spacedevs_news_prod.py
+++ spacedevs/assets/prod_news/spacedevs_news_prod.py
@@ -107,7 +107,7 @@ def posts_prod(articles_prod, blogs_prod, reports_prod):
                 }
                 #my_logger.info(ghost_body)
                 r = requests.post(ghost_url, json=ghost_body, headers=headers)
-                my_logger.info(r.text)
+                my_logger.info(r)
         except:
             my_logger.error("Couldn't post article!")
 
